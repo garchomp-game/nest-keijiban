be4719020fede09714016aadfb808644bab01e3d
garchomp-game <otoka.japanesedrum@gmail.com>
2019-08-19 19:47:06 +0900

    必要なファイル、パッケージ準備

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BoardModule } from './board/board.module';
+import { ChatModule } from './chat/chat.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, BoardModule, ChatModule,
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
